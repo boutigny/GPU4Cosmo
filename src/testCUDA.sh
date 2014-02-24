c59bf5525dbffd61a2bb0c69011342d8c44b409b
Fabio Hernandez <fairnandez@gmail.com>
2014-02-24 09:34:56 +0100

    Fix typo in executable names

--- src/testCUDA.sh
+++ src/testCUDA.sh
@@ -8,4 +8,4 @@ outputDir=${topDir}/output
 
 inputFile="Aardvark/Catalog_v1.0/truth_oscillationcorrected_unrotated/Aardvark_v1.0c_truth.190.root"
 outputFile="reference_CUDA.root"
-${binDir}/cudaRef  ${dataDir}/${inputFile}  ${outputDir}/${outputFile}
+${binDir}/cudaref  ${dataDir}/${inputFile}  ${outputDir}/${outputFile}
